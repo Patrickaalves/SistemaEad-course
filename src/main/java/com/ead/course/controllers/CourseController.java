da329fbee94260c91fef0873c805ddd7256b7a2e
Patrickaalves <patrickalvessc@gmail.com>
2024-11-16 17:52:08 -0300

    Adicionando configuracao global de cors

--- src/main/java/com/ead/course/controllers/CourseController.java
+++ src/main/java/com/ead/course/controllers/CourseController.java
@@ -1,9 +1,15 @@
 package com.ead.course.controllers;
 
+import com.ead.course.services.CourseService;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/course")
+@RequestMapping("/courses")
 public class CourseController {
+    final CourseService courseService;
+
+    public CourseController(CourseService courseService) {
+        this.courseService = courseService;
+    }
 }
